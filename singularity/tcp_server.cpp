614dad62baaf6800f22ac33f51cb9fb2b640f784
skunichetty <sachchitku@gmail.com>
2024-06-07 19:23:45 -0400

    Added TCP Server header + some implementation

---
+++ singularity/tcp_server.cpp
@@ -0,0 +1,21 @@
+#include "tcp_server.hpp"
+
+#include <unistd.h>
+
+#include <cstring>
+#include <limits>
+#include <stdexcept>
+
+#include "utils.hpp"
+
+TCPServer::TCPServer(uint32_t port) : _port{port}, _sock_fd{-1} {
+    uint32_t max_port =
+        static_cast<uint32_t>(std::numeric_limits<uint16_t>::max());
+    if (_port < 0 || _port > max_port) {
+        throw std::invalid_argument(build_string("Invalid port number ", port,
+                                                 ", expected in range [0,",
+                                                 max_port, "]"));
+    }
+}
+
+TCPServer::~TCPServer() { close(_sock_fd); }
