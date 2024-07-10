1f78a8f01764e27b7e5431486bd8c43054aa8554
skunichetty <sachchitku@gmail.com>
2024-07-09 21:07:58 -0400

    Added OpenBLAS Dependency

--- singularity/main.cpp
+++ singularity/main.cpp
@@ -1,5 +1,17 @@
+#include <cblas.h>
+
 #include <iostream>
 
-int main() { 
+int main() {
     std::cout << "Hello from singularity!" << std::endl;
+
+    int i = 0;
+    double A[4] = {1.0, 2.0, 1.0, -3.0};
+    double B[4] = {2.0, 0.0, 0.0, 0.5};
+    double C[4] = {.5, .5, .5, .5};
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 2, 2, 2, 1, A, 2, B,
+                2, 0, C, 2);
+
+    for (i = 0; i < 4; i++) printf("%lf ", C[i]);
+    printf("\n");
 }
